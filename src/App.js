4557248ad293741b1946b558d6ad99335247f4c9
Matthew Taylor <matthew10taylor@gmail.com>
2023-03-17 15:06:48 -0400

    Implement basic UI, Format dates
    Added some basic visuals to test UI design. Convert y/m/d format to en-US

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   const [dataObject, setData] = useState();
 
   useEffect(() => {
-    fetch(`https://api.open-meteo.com/v1/forecast?latitude=38.50&longitude=-81.50&daily=temperature_2m_max,temperature_2m_min,precipitation_probability_max&temperature_unit=fahrenheit&timezone=America%2FNew_York`)
+    fetch(`https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m,rain&daily=temperature_2m_max,temperature_2m_min,uv_index_max,precipitation_hours,precipitation_probability_max&temperature_unit=fahrenheit&windspeed_unit=ms&precipitation_unit=inch&forecast_days=14&timezone=America%2FNew_York`)
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
@@ -20,6 +20,7 @@ function App() {
 
   return (
     <>
+      <h1>Lawn Forecast</h1>
       <Forecast data={dataObject} />
     </>
   );
--- src/Forecast.js
+++ src/Forecast.js
@@ -5,33 +5,77 @@ import {
     AccordionItemHeading,
     AccordionItemButton,
     AccordionItemPanel,
-  } from 'react-accessible-accordion';
+} from 'react-accessible-accordion';
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 import './Forecast.css'
 
+function isPrecipNull(data) {
+    if (data) {
+        return `${data}%`;
+    }
+    else {
+        return '?';
+    }
+};
+
+function convertDate(date) {
+    var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    return new Date(date).toLocaleDateString("en-US", options);
+};
+
 export default class Forecast extends Component {
-  render() {
-    return (
-        <div>
-        <h1>Lawn Forecast</h1>
-        {this.props.data && this.props.data.daily && this.props.data.daily.temperature_2m_max && (
-          <Accordion>
-            {this.props.data.daily.time.map((day, index) => (
-              <AccordionItem key={day}>
-                <AccordionItemHeading>
-                  <AccordionItemButton>
-                    {day}
-                  </AccordionItemButton>
-                </AccordionItemHeading>
-                <AccordionItemPanel>
-                  High Temperature: {this.props.data.daily.temperature_2m_max[index]}°F
-                  <br></br>
-                  Low Temperature: {this.props.data.daily.temperature_2m_min[index]}°F
-                </AccordionItemPanel>
-              </AccordionItem>
-            ))}
-          </Accordion>
-        )}
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div>
+                {this.props.data && this.props.data.daily && this.props.data.daily.temperature_2m_max && (
+                    <Accordion>
+                        {this.props.data.daily.time.map((day, index) => (
+                            <AccordionItem key={day}>
+                                <AccordionItemHeading>
+                                    <AccordionItemButton>
+                                        {convertDate(day)}
+                                    </AccordionItemButton>
+                                </AccordionItemHeading>
+                                <AccordionItemPanel>
+                                    <table>
+                                        <tr>
+                                            <td>
+                                                High
+                                            </td>
+                                            <td>
+                                                Low
+                                            </td>
+                                            <td>
+                                                Rain
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                <CircularProgressbar value={100} text={`${Math.ceil(this.props.data.daily.temperature_2m_max[index])}°F`}
+                                                    styles={buildStyles({
+                                                        pathColor: '#FF6961',
+                                                        textColor: 'black'
+                                                    })} />
+                                            </td>
+                                            <td>
+                                                <CircularProgressbar value={100} text={`${Math.ceil(this.props.data.daily.temperature_2m_min[index])}°F`}
+                                                    styles={buildStyles({
+                                                        pathColor: '#ADD8E6',
+                                                        textColor: 'black'
+                                                    })} />
+                                            </td>
+                                            <td>
+                                                <CircularProgressbar value={this.props.data.daily.precipitation_probability_max[index]} text={isPrecipNull(this.props.data.daily.precipitation_probability_max[index])} />
+                                            </td>
+                                        </tr>
+                                    </table>
+                                </AccordionItemPanel>
+                            </AccordionItem>
+                        ))}
+                    </Accordion>
+                )}
+            </div>
+        )
+    }
 }
