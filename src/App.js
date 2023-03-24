f23f11ced0a227539cce8f26bdbcdf39af16fa75
Matthew Taylor <matthew10taylor@gmail.com>
2023-03-24 14:49:02 -0400

    Import zipcode data locally

--- src/App.js
+++ src/App.js
@@ -1,35 +1,44 @@
 import React, { useState } from 'react';
 import Forecast from './Forecast'
 import './App.css';
+import zipcodes from './assets/zipcodes.json';
 
 function App() {
 
-  const [dataObject, setData] = useState();
+  const [weatherDataObject, setWeatherData] = useState();
+  const [locationDataObject, setLocationData] = useState({ city: '', state: '' });
 
   async function fetchData() {
-    let zipcode = document.getElementById('zipcode').value;
-    let latitude = 0;
-    let longitude = 0;
+    let input_zipcode = Number(document.getElementById('zipcode').value);
+    let latitude, longitude = 0;
 
-    const coordResponse = await fetch(`http://api.geonames.org/postalCodeSearch?postalcode=${zipcode}&country=US&username=lawnforecast`);
-    const coordData = await coordResponse.text();
-    const parsedData = new window.DOMParser().parseFromString(coordData, "text/xml");
-    latitude = parsedData.getElementsByTagName("lat")[0].textContent;
-    longitude = parsedData.getElementsByTagName("lng")[0].textContent;
-    console.log(latitude);
-    console.log(longitude);
+    for (let i = 0; i < zipcodes.length; i++) {
+      if (zipcodes[i].zipcode === input_zipcode) {
+        latitude = zipcodes[i].latitude;
+        longitude = zipcodes[i].longitude;
+        setLocationData({ city: zipcodes[i].city, state: zipcodes[i].state });
+      }
+    }
 
-    const weatherResponse = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,rain&daily=temperature_2m_max,temperature_2m_min,uv_index_max,precipitation_hours,precipitation_probability_max&temperature_unit=fahrenheit&windspeed_unit=ms&precipitation_unit=inch&forecast_days=10&timezone=America%2FNew_York`);
-    const weatherData = await weatherResponse.json();
-    console.log(weatherData);
-    setData(weatherData);
+    await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,rain&daily=temperature_2m_max,temperature_2m_min,uv_index_max,precipitation_hours,precipitation_probability_max&temperature_unit=fahrenheit&windspeed_unit=ms&precipitation_unit=inch&forecast_days=10&timezone=America%2FNew_York`)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        else {
+          alert('Unable to fetch data for the provided zipcode. Please double check your input.');
+        }
+      }).then((json) => {
+        setWeatherData(json);
+      })
   }
 
   return (
     <>
       <input id='zipcode'></input>
       <button onClick={() => { fetchData() }}>Fetch Data</button>
-      <Forecast data={dataObject} />
+      <p>Displaying data for: {`${locationDataObject.city}, ${locationDataObject.state}`}</p>
+      <Forecast data={weatherDataObject} />
     </>
   );
 }
